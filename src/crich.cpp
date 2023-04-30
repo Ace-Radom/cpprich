ed4802d53cf93784c049b3dd03d3e66f8b5f5de9
Ace_Radom <sichenradomlyu@gmail.com>
2023-04-30 17:06:05 +0200

    add NULL-input judgement with C API

--- src/crich.cpp
+++ src/crich.cpp
@@ -1,5 +1,7 @@
 #include"crich.h"
 
+#define JUDGE_NULL( str ) ( str == NULL ) ? "" : str
+
 void cpprich_init( cpprich_colortype_t __colortype ){
     color_type = __colortype;
     return;
@@ -14,14 +16,14 @@ void crprint( const char* __format , ... ){
 }
 
 void crstatus( const char* __text , const char* __status_display_sytle ){
-    rstatus( std::string( __text ) , std::string( __status_display_sytle ) );
+    rstatus( std::string( JUDGE_NULL( __text ) ) , std::string( JUDGE_NULL( __status_display_sytle ) ) );
     return;
 }
 
 void crpanel( const char* __title , const char* __subtitle , const unsigned int __width , const char* __sytle , const unsigned int __line_num , ... ){
     va_list lines;
     va_start( lines , __line_num );
-    rpanel( std::string(  __title ) , std::string( __subtitle ) , __width , std::string( __sytle ) , __line_num , lines );
+    rpanel( std::string( JUDGE_NULL( __title ) ) , std::string( JUDGE_NULL( __subtitle ) ) , __width , std::string( JUDGE_NULL( __sytle ) ) , __line_num , lines );
     va_end( lines );
     return;
 }
